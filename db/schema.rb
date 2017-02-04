02834f4326e3910f06f384104079481bc8df8ded
Aaron Janke <aaronljanke@gmail.com>
2017-02-04 16:20:34 -0600

    jjj

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204212511) do
+ActiveRecord::Schema.define(version: 20170204221918) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 20170204212511) do
 
   create_table "words", force: :cascade do |t|
     t.string   "word_listed"
+    t.text     "meaning"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_words_on_user_id"
   end
 
 end
