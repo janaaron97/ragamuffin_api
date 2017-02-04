190a136e380a07ee5b295b7bf5f4d9e094dba60b
Aaron Janke <aaronljanke@gmail.com>
2017-02-04 15:36:54 -0600

    removed migration files

--- db/migrate/20170204212051_new_table.rb
+++
@@ -1,4 +0,0 @@
-class NewTable < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
--- db/migrate/drop_words_table.rb
+++
@@ -1,9 +0,0 @@
-class DropWordsTable < ActiveRecord::Migration
-  def up
-    drop_table :words
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
