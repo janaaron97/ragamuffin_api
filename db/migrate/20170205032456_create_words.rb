f91005299941df16cdb996ba1ad8577e1f387fdb
Aaron Janke <aaronljanke@gmail.com>
2017-02-04 21:25:46 -0600

    fdasf

--- db/migrate/20170205032456_create_words.rb
+++ db/migrate/20170205032456_create_words.rb
@@ -3,7 +3,7 @@ class CreateWords < ActiveRecord::Migration[5.0]
     create_table :words do |t|
       t.string :word_listed
       t.text :meaning
-      t.references :user_id, foreign_key: true
+      t.integer :user_id
 
       t.timestamps
     end
